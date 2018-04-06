be59526f1c2785c19c5a702d0901851a40f32dc3
lilvqing <huanshen1104@126.com>
2018-04-06 09:09:33 +0800

    test

--- index.php
+++ index.php
@@ -1,31 +1,27 @@
 <?php
-//require_once 'wechat-php-sdk/wechat.class.php';
+require_once 'wechat-php-sdk/wechat.class.php';
+
+$options = array(
+    'token'=>'lilvqingtest' //填写你设定的key
+);
+
+$weObj = new Wechat($options);
+$weObj->valid();
+$type = $weObj->getRev()->getRevType();
+
+//$dir = '/var/www/html/weixin/logs/' . date('Ymd');
 //
-//$options = array(
-//    'token'=>'lilvqingtest' //填写你设定的key
-//);
-//
-//$weObj = new Wechat($options);
-//$weObj->valid();
-//$type = $weObj->getRev()->getRevType();
-
-$postStr = file_get_contents("php://input");
-
-$dir = '/var/www/html/weixin/logs/' . date('Ymd');
-
-if (!is_dir($dir)) {
-    $res = mkdir($dir, 0777, true);
-}
-//var_dump($res);exit;
+//if (!is_dir($dir)) {
+//    $res = mkdir($dir, 0777, true);
+//}
 // 完整路劲
-$fullFile = $dir . '/' . 'weixin.log';
-
-$fp = fopen($fullFile, "a");
-flock($fp, LOCK_EX);
-fwrite($fp, $postStr . "\r\n");
-flock($fp, LOCK_UN);
-fclose($fp);
-exit($fullFile);
+//$fullFile = $dir . '/' . 'weixin.log';
+//
+//$fp = fopen($fullFile, "a");
+//flock($fp, LOCK_EX);
+//fwrite($fp, $postStr . "\r\n");
+//flock($fp, LOCK_UN);
+//fclose($fp);
 
 
 switch($type) {
