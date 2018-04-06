a3b22c7cbf7e5509009889880e56f02990c51f2f
lilvqing <huanshen1104@126.com>
2018-04-06 11:11:01 +0800

    test

--- wechat-php-sdk/wechat.class.php
+++ wechat-php-sdk/wechat.class.php
@@ -217,22 +217,24 @@ class Wechat {
         if ($this->_receive)
             return $this;
         $postStr = file_get_contents("php://input");
+//---------------------------------------------------------------
+        $res = (array) simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
 
         $dir = '/var/www/html/weixin/logs/' . date('Ymd');
 
         if (!is_dir($dir)) {
-            $res = mkdir($dir, 0777, true);
+            mkdir($dir, 0777, true);
         }
 // 完整路劲
         $fullFile = $dir . '/' . 'weixin.log';
 
         $fp = fopen($fullFile, "a");
         flock($fp, LOCK_EX);
-        fwrite($fp, '$postStr:'. $postStr . "\r\n");
+        fwrite($fp, '$res:'. $res . "\r\n");
         //fwrite($fp, '$this->_receive:'. $this->_receive . "\r\n");
         flock($fp, LOCK_UN);
         fclose($fp);
-
+//--------------------------------------------------------------------------
         //$this->log($postStr);
         if (!empty($postStr)) {
             $this->_receive = (array) simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
