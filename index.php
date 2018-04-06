4a3d54e6f031f8bf831048045fa2a81ed0a1b31d
lilvqing <huanshen1104@126.com>
2018-04-06 09:48:23 +0800

    test

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $options = array(
     'token'=>'lilvqingtest' //填写你设定的key
 );
 
-$weObj = new Wechat($options);
+//$weObj = new Wechat($options);
 
 //$weObj->valid();
-$type = $weObj->getRev()->getRevType();
+//$type = $weObj->getRev()->getRevType();
 
 $postStr = file_get_contents("php://input");
 
@@ -22,7 +22,7 @@ $fullFile = $dir . '/' . 'weixin.log';
 
 $fp = fopen($fullFile, "a");
 flock($fp, LOCK_EX);
-fwrite($fp, '$type:'. $type . "\r\n");
+fwrite($fp, '$postStr:'. $postStr . "\r\n");
 flock($fp, LOCK_UN);
 fclose($fp);
 
